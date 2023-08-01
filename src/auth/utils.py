679604bed20ba8fec8feb05d24cdb50095b8630d
xcapt0 <townhor@mail.ru>
2023-08-01 21:32:40 +0500

    refactoring

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,10 +1,16 @@
 from fastapi import Depends
+from fastapi_users.authentication import JWTStrategy
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from config import SECRET_AUTH
 from .models import User
 from database import get_async_session
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     return SQLAlchemyUserDatabase(session, User)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET_AUTH, lifetime_seconds=3600)
